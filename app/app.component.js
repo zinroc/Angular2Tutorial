39c2db8142dc5a2a718329383476f5a8f5c404c5
zinroc <admin@Admins-MacBook-Air-3.local>
2017-01-09 14:21:54 -0500

    added static list

--- app/app.component.js
+++ app/app.component.js
@@ -15,9 +15,14 @@ var Hero = (function () {
     return Hero;
 }());
 exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: "sdfijas" },
+    { id: 12, name: "John" }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
         this.hero = {
             id: 1,
             name: 'windstorm'
@@ -26,7 +31,9 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1> {{title}}</h1>\n  <h2> {{hero.name}} details!</h2> \n  <div><label>id:</label> {{hero.id}}</div>\n  <div>\n  \t<label> name: </label>\n  \t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n  </div>",
+            template: "\n\t<h1> {{title}}</h1>\n\t<h2>My Heroes</h2>\n\t<ul class=\"heroes\">\n\t\t<li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\">\n\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t</li>\n\t</ul>\n\t",
+            styles: ["\n\t  .selected {\n\t    background-color: #CFD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes {\n\t    margin: 0 0 2em 0;\n\t    list-style-type: none;\n\t    padding: 0;\n\t    width: 15em;\n\t  }\n\t  .heroes li {\n\t    cursor: pointer;\n\t    position: relative;\n\t    left: 0;\n\t    background-color: #EEE;\n\t    margin: .5em;\n\t    padding: .3em 0;\n\t    height: 1.6em;\n\t    border-radius: 4px;\n\t  }\n\t  .heroes li.selected:hover {\n\t    background-color: #BBD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes li:hover {\n\t    color: #607D8B;\n\t    background-color: #DDD;\n\t    left: .1em;\n\t  }\n\t  .heroes .text {\n\t    position: relative;\n\t    top: -3px;\n\t  }\n\t  .heroes .badge {\n\t    display: inline-block;\n\t    font-size: small;\n\t    color: white;\n\t    padding: 0.8em 0.7em 0 0.7em;\n\t    background-color: #607D8B;\n\t    line-height: 1em;\n\t    position: relative;\n\t    left: -1px;\n\t    top: -4px;\n\t    height: 1.8em;\n\t    margin-right: .8em;\n\t    border-radius: 4px 0 0 4px;\n\t  }"
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
